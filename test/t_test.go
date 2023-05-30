eab6cc4fc8c0fc4e305add1167b7b16550774ebd
longxiao <hellcox@163.com>
2023-05-30 18:16:17 +0800

    优化连接速率，弃用令牌桶

--- test/t_test.go
+++ test/t_test.go
@@ -4,10 +4,32 @@ import (
 	"fmt"
 	"math/big"
 	"testing"
+	"time"
 )
 
 func Test_a(t *testing.T) {
-	val := big.NewInt(0xffffffffffffffffffffffffffffffff)
+	val := big.NewInt(0xfffffff)
 
 	fmt.Printf("%s的十进制为%s \n", val.String(), val.String())
+}
+
+var Count int
+var CountOri int
+var sTime time.Duration
+
+func Test_b(t *testing.T) {
+	go func() {
+		for true {
+			Now := Count
+			fmt.Printf("Count=%d,%d, sTime=%v\n", Now, Now-CountOri, sTime)
+			CountOri = Now
+			time.Sleep(time.Second)
+		}
+	}()
+	for true {
+		Count++
+		start := time.Now()
+		time.Sleep(1 * time.Microsecond)
+		sTime = time.Now().Sub(start)
+	}
 }
